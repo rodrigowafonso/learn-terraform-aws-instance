549ff2c7bcab52b7cb63410667655d40274a1712
rodrigowafonso <rodrigowalterafonso@gmail.com>
2024-12-04 21:26:11 -0300

    Ingregrando Terraform na Pipeline

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-output "id_instancia" {
-  description = "ID of the Instância"
-  value       = aws_instance.learn-terraform-aws.id
-}
+# output "id_instancia" {
+#   description = "ID of the Instância"
+#   value       = aws_instance.learn-terraform-aws.id
+# }
 
-output "ip_publico_instancia" {
-  description = "IP público da Instância"
-  value       = aws_instance.learn-terraform-aws.public_ip
-}
+# output "ip_publico_instancia" {
+#   description = "IP público da Instância"
+#   value       = aws_instance.learn-terraform-aws.public_ip
+# }
 
 output "ip_publico_instancia_canonical" {
   description = "IP público da Instância"
